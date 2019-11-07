9d2f07e6799fe25efe56202fd89e867d20487fc7
CAS OLEJNICZAK <CASOLEJNICZAK@GMAIL.COM>
2019-11-06 19:19:42 -0800

    finished problem 1

--- class-05/lab-a/starter-code/app.js
+++ class-05/lab-a/starter-code/app.js
@@ -8,12 +8,14 @@ Write a function called sum() that takes in two numbers as arguments and then re
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testSum() function below and check the console to see if the test passes.*/
 
 // Write your code here
-function sum(a, b) { //eslint-disable-line
-
+function sum(num1, num2) { //eslint-disable-line
+    var sumOfNums = (num1 + num2)
+    return [sumOfNums, "The sum of " + num1 + " and " + num2 + " is " + sumOfNums + "."];
 }
+// sum(4, 7);
 
 // Here is the test for sum(); uncomment it to run it
-// testSum(4, 7);
+testSum(4, 7);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
